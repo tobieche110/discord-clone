081685cf118593ccb7ce32511374421968adc832
tobias <tobiasechenique>
2023-12-09 18:56:11 -0300

    Add initial modal

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -33,6 +34,12 @@ const formSchema = z.object({
 });
 
 export const InitialModal = () => {
+    const [isMounted, setIsMounted] =useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -45,7 +52,11 @@ export const InitialModal = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values);
-    };
+    }
+
+    if (!isMounted) {
+        return null;
+    }
 
     return (
         <Dialog open>
@@ -60,7 +71,40 @@ export const InitialModal = () => {
                     </DialogDescription>
                 </DialogHeader>
                 <Form {...form}>
-                    
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8"
+                    >
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                IMAGE UPLOAD
+                            </div>
+
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                                            Server name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0  focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Enter server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className="bg-gray-100 px-6 py-4">
+                            <Button variant="primary" disabled={isLoading}>Create</Button>
+                        </DialogFooter>
+                    </form>
                 </Form>
             </DialogContent>
         </Dialog>
